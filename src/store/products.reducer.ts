682c594d9f1305d5ff16159780ba526cbbaa88f4
Imageumisery <imageumisery@gmail.com>
2023-08-08 07:50:34 +0500

    ShoppingCart part done

--- src/store/products.reducer.ts
+++ src/store/products.reducer.ts
@@ -1,36 +1,36 @@
-import { ShoppingCartItem } from "../types/ShoppingCartItem"
-import { ProductActionTypes } from "./action-types"
-import { ProductActions } from "./products.actions"
+import { ShoppingCartItem } from "../types/ShoppingCartItem";
+import { ProductActionTypes } from "./action-types";
+import { ProductActions } from "./products.actions";
 
 interface ProductState {
-    shoppingCart: ShoppingCartItem[]
+    shoppingCart: ShoppingCartItem[];
 }
 
 const initialState: ProductState = {
-    shoppingCart: []
-}
+    shoppingCart: [],
+};
 
 export const productsReducer = (state = initialState, action: ProductActions): ProductState => {
     switch (action.type) {
         case ProductActionTypes.Add:
-            const productIndex = state.shoppingCart.findIndex((item) => item.product.id === action.payload.product.id);
+            const productIndex = state.shoppingCart.findIndex(
+                (item) => item.product.id === action.payload.product.id
+            );
             if (productIndex < 0) {
                 const newCart = [...state.shoppingCart, action.payload];
-                return {...state, shoppingCart: newCart};
-            } 
+                return { ...state, shoppingCart: newCart };
+            }
             const originalProduct = state.shoppingCart[productIndex];
-            const modifiedProduct: ShoppingCartItem = {...originalProduct, amount: ++originalProduct.amount}
+            const modifiedProduct: ShoppingCartItem = {
+                ...originalProduct,
+                amount: ++originalProduct.amount,
+            };
             state.shoppingCart[productIndex] = modifiedProduct;
-            return {...state, shoppingCart: [...state.shoppingCart]}
+            return { ...state, shoppingCart: [...state.shoppingCart] };
         case ProductActionTypes.Remove:
-            const newCart2 = state.shoppingCart.filter(({product}) => product.id !== action.payload.id)
-            return {...state, shoppingCart: newCart2}
+            const newCart2 = state.shoppingCart.filter(({ product }) => product.id !== action.payload.id);
+            return { ...state, shoppingCart: newCart2 };
         default:
-            return state
+            return state;
     }
-}
-
-
-const a = [{user: 'shaha'}]
-
-const b = [...a]
+};
